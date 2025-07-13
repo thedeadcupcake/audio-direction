44788dbefdfda5e40fc062937f152470e9d35a10
thedeadcupcake5 <flettzach@gmail.com>
2025-07-12 22:12:06 -0700

    Implemented recording of system audio output Uses vb audio virtual cable to capture output while maintaining functionality of the real output

--- main.py
+++ main.py
@@ -1,7 +1,50 @@
+import pyaudio 
+import numpy as np
+import wave
+
+DEVICE_NAME = ""
+CHUNK = 256
+FORMAT = None
+CHANNELS = 2
+RATE = 44100
+RECORD_SECONDS = 5
+WAVE_OUTPUT_FILENAME = "output.wav"
+
+
+pya = pyaudio.PyAudio()
+
+
+def get_virtual_cable_index():
+    for i in range(pya.get_device_count()):
+        if "CABLE Output" in pya.get_device_info_by_index(i)["name"]:
+            return i
+        
+    print("No virtual cable found!")
+
+    return None
 
 
 def main():
-    print("hi")
+    with wave.open("output.wav", "wb") as wf:
+        wf.setnchannels(CHANNELS)
+        wf.setsampwidth(sampwidth=pyaudio.get_sample_size(pyaudio.paInt16))
+        wf.setframerate(RATE)
+        
+        print("Recording...")
+
+        stream = pya.open(rate=RATE, channels=CHANNELS, format=pyaudio.paInt16, input_device_index=get_virtual_cable_index(), input=True, frames_per_buffer=CHUNK) # 6 is my virtual cable driver
+
+        for i in range((RATE//CHUNK) * RECORD_SECONDS):
+            data = stream.read(CHUNK)
+
+            wf.writeframes(data=data)
+    
+
+        print("Finished")
+
+        
+        pya.terminate()
+       
 
 
 if __name__ == "__main__":
